1cb5b432d066594edca5c32bbfeab8a0876d9324
Eugene Poltorakov <jslayer@gmail.com>
2013-07-10 22:38:51 +0300

    basic tree parsing

--- src/a2/js/a2-module.js
+++ src/a2/js/a2-module.js
@@ -1,22 +1,76 @@
 YUI.add('a2', function (Y, NAME) {
 
+    var A = Y.Array;
+
+    var L = Y.Lang;
+
     var A2 = function A2() {
         A2.superclass.constructor.apply(this, arguments);
     };
+
+    var EMPTY_RE = /^\s*$/;
 
     A2.NAME = 'A2';
 
     Y.extend(A2, Y.Base, {
         initializer : function() {
-            console.log('initialize');
+            var src = this.get('srcNode').getDOMNode();
 
-            console.log(this.get('strings.textValueLabel'));
-            console.log(this.get('form.textValue'));
+            console.time('tree');
+
+            var tree = this.parse(src);
+
+            console.timeEnd('tree');
+        },
+        parse : function(node, parent) {
+            var tree = {};
+
+            var element = {
+                node   : node,
+                parent : parent || false
+            };
+
+            var dive;
+
+            dive = this.pre(element);
+
+            if (dive && node.childNodes && node.childNodes.length > 0) {
+                element.children = [];
+
+                A.each(node.childNodes, function(child) {
+                    var go = false, parsed;
+
+                    switch(child.nodeType) {
+                        case 1://ELEMENT_NODE
+                            go = true;
+                            break;
+                        case 3://TEXT_NODE
+                            if (!EMPTY_RE.test(child.textContent)) {
+                                go = true;
+                            }
+                            break;
+                    }
+
+                    if (go) {
+                        element.children.push(
+                            this.parse(child, element)
+                        );
+                    }
+                }, this);
+            }
+
+            return element;
+        },
+        pre : function(element) {
+            console.log('PRE     ', element);
+            return true;
         }
     }, {
         ATTRS : {
             srcNode : {
-                value : null
+                setter : function(v) {
+                    return L.type(v) ? Y.one(v) : v;
+                }
             }
         }
     });
@@ -38,6 +92,7 @@ YUI.add('a2', function (Y, NAME) {
     //process extension
 
     //& extension
+
 
 
 
