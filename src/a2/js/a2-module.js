6460f1ff65074b0c768efcc10afe5a8440f790f5
Eugene Poltorakov <jslayer@gmail.com>
2013-07-11 00:06:23 +0300

    PluginTextModel

--- src/a2/js/a2-module.js
+++ src/a2/js/a2-module.js
@@ -14,11 +14,24 @@ YUI.add('a2', function (Y, NAME) {
 
     Y.extend(A2, Y.Base, {
         initializer : function() {
+            this.publish('pre', {
+                emitFacade: true
+            });
+
+            this.publish('main', {
+                emitFacade: true
+            });
+
+            this.plug(PluginTextModel);
+            this.plug(PluginNodeModel);
+
             var src = this.get('srcNode').getDOMNode();
 
             console.time('tree');
 
-            var tree = this.parse(src);
+            this.processTree(
+                this.parse(src)
+            );
 
             console.timeEnd('tree');
         },
@@ -32,7 +45,9 @@ YUI.add('a2', function (Y, NAME) {
 
             var dive;
 
-            dive = this.pre(element);
+            dive = this.fire('pre', {
+                element : element
+            });
 
             if (dive && node.childNodes && node.childNodes.length > 0) {
                 element.children = [];
@@ -61,9 +76,17 @@ YUI.add('a2', function (Y, NAME) {
 
             return element;
         },
-        pre : function(element) {
-            console.log('PRE     ', element);
-            return true;
+        processTree : function(tree) {
+            this.fire('main', {
+                element : tree
+            });
+            if (tree.children) {
+                A.each(tree.children, function(child) {
+                    this.fire('main', {
+                        element : child
+                    });
+                }, this);
+            }
         }
     }, {
         ATTRS : {
@@ -71,9 +94,119 @@ YUI.add('a2', function (Y, NAME) {
                 setter : function(v) {
                     return L.type(v) ? Y.one(v) : v;
                 }
+            },
+            value1 : {
+                value : 1
+            },
+            value2 : {
+                value : 2
             }
         }
     });
+
+    function PluginTextModel(cfg){
+        this.host = cfg.host;
+        PluginTextModel.superclass.constructor.apply(this, arguments);
+    }
+
+    Y.extend(PluginTextModel, Y.Plugin.Base, {
+        initializer : function() {
+            this.onHostEvent('main', function(e) {
+                console.log('evt', e);
+                if (e.element.node.nodeType === 3) {
+                    this.main(e.element, e);
+                }
+            });
+        },
+        main : function(element, e) {
+            var text = L.trim(element.node.wholeText);
+
+            var frag = document.createDocumentFragment();
+
+            A.each(text.split(this.expr.split).filter(function(item) {
+                return !!item;
+            }), function(item) {
+                var node;
+
+                if (this.expr.test.test(item)) {
+                    var name = item.match(this.expr.parse).pop();
+
+                    node = document.createTextNode(this.host.get(name));
+
+                    this.afterHostEvent(name + 'Change', function(e) {
+                        node.nodeValue = e.newVal;
+                    });
+                }
+                else {
+                    node = document.createTextNode(item);
+                }
+                frag.appendChild(node);
+            }, this);
+
+            element.node.parentNode.replaceChild(frag, element.node);
+        },
+        expr : {
+            split : /({{\s*\w+\s*}})/g,
+            test  : /^{{\s*\w+\s*}}$/g,
+            parse : /\w+/g
+        }
+    }, {
+        NS : 'PluginTextModel'
+    });
+
+    function PluginNodeModel() {
+        PluginNodeModel.superclass.constructor.apply(this, arguments);
+    }
+
+    Y.extend(PluginNodeModel, Y.Plugin.Base, {
+        initializer : function() {
+//            this.onHostEvent('pre', function(e) {
+//                if (e.element.node.nodeType === 1) {
+//                    this.pre(e.element, e);
+//                }
+//            });
+        },
+        pre : function() {
+
+        }
+    }, {
+        NS : 'PluginNodeModel'
+    });
+
+    /**
+     * Basic text plugin
+     */
+    /*function PluginTextBasic(cfg) {
+        console.log(cfg);
+        PluginTextBasic.superclass.constructor.apply(this, arguments);
+    }
+
+    Y.extend(PluginTextBasic, Y.Plugin.Base, {
+       initializer : function() {
+           this.onHostEvent('pre', function(e) {
+             console.log('PTB', e);
+           });
+       }
+    }, {
+        NS: 'textBasic'
+    });
+
+    *//**
+     * Basic model plugin
+     *//*
+    function PluginNodeBasic(cfg) {
+        PluginNodeBasic.superclass.constructor.apply(this, arguments);
+    }
+
+    Y.extend(PluginNodeBasic, Y.Plugin.Base, {
+        initializer : function() {
+            this.onHostEvent('pre', function(e) {
+                console.log('PNB', e);
+            });
+        }
+    }, {
+        NS : 'nodeBasic'
+    });*/
 
     /**
      * We should go through dom tree (with text nodes)
@@ -98,4 +231,4 @@ YUI.add('a2', function (Y, NAME) {
 
     Y.A2 = A2;
 
-}, '1.0.0-dev', {"requires": ["base", 'node']});
+}, '1.0.0-dev', {"requires": ["base", 'node', 'plugin']});
