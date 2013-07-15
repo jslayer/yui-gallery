e4fdba2ff390dc8426e2158a58539881d2dda7dc
Eugene Poltorakov <jslayer@gmail.com>
2013-07-15 13:22:09 +0300

    simple token parser

--- src/a2/assets/2.js
+++ src/a2/assets/2.js
@@ -1,9 +1,13 @@
 #! /usr/bin/node
 
-lex('1+1');
+//lex(' "A" + "B" ');
+//lex('1+1');
+lex('"+A123" + "Test"');
 
 function lex(str) {
     var l, i, ch, s, S, ts, c, lsc;
+
+    str += '\n';
 
     S = {
         normal   : 0,
@@ -17,64 +21,65 @@ function lex(str) {
     ts = [];
     s = S.normal;
 
-
     while(i < l) {
-        ch = str.substr(i, 1);//charAt ?
-        i++;
+        ch = str.substr(i,1);
 
         console.log(i + ' : ' + ch + ' : ' + s);
 
         switch(s) {
             case S.normal:
                 if (~' \r\t\n\v\u00A0'.indexOf(ch)) {//whitespace
+                    i++;
                     continue;
                 }
                 if (new RegExp('[0-9a-z$.]').test(ch)){
-                    console.log('omg');
                     s = S.literal;
                     c = ch;
+                    i++;
                     continue;
                 }
                 if (~'\'"'.indexOf(ch)) {//string start
                     s = S.str;
                     lsc = ch;
                     c = '';
+                    i++;
                     continue;
                 }
-                if (~'+-*/~!%&|'.indexOf(ch)) {//+-*/
+                if (~'+-*//*~!%&|'.indexOf(ch)) {//+-*//*
                     c = ch;
                     s = S.operator;
+                    i++;
                     continue;
                 }
                 break;
             case S.literal:
                 if (new RegExp('[0-9a-z$.]').test(ch)) {
                     c += ch;
+                    i++;
                     continue;
                 }
                 else {
                     ts.push(c);
                     s = S.normal;
-                    i--;
                     continue;
                 }
                 break;
             case S.str:
                 if (ch !== lsc) {
                     c += ch;
+                    i++;
+                    continue;
                 }
                 else {
                     ts.push(c);
                     s = S.normal;
-                    i--;
+                    i++;
                 }
                 break;
             case S.operator:
                 if ((c === '|' && ch === '|') || (c === '&' && ch === '&')) {
                     c += ch;
-                }
-                else {
-                    i--;
+                    i++;
                 }
                 ts.push(c);
                 s = S.normal;
